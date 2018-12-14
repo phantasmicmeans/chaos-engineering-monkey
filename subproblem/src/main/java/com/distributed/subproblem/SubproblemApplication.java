2fa2871836b9b8817877936cb537276e0849a43e
phantasmicmeans <dlaak0630@naver.com>
2018-12-15 03:32:10 +0900

    init

---
+++ subproblem/src/main/java/com/distributed/subproblem/SubproblemApplication.java
@@ -0,0 +1,14 @@
+package com.distributed.subproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubproblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubproblemApplication.class, args);
+    }
+
+}
+
