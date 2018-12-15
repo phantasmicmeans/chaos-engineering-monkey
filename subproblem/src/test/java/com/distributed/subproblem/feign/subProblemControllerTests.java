34f52b056145b82aa0a802f6973516d59eef2b03
phantasmicmeans <dlaak0630@naver.com>
2018-12-16 04:35:26 +0900

    add feign subProblem client rest controller, init test logic

---
+++ subproblem/src/test/java/com/distributed/subproblem/feign/subProblemControllerTests.java
@@ -0,0 +1,22 @@
+package com.distributed.subproblem.feign;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class subProblemControllerTests {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Autowired
+    subProblemService subProblemService;
+
+}
+
