7377cd520aa98d54e5fd3bf8ca146a698e907323
phantasmicmeans <dlaak0630@naver.com>
2018-12-18 03:01:56 +0900

    initial commit

---
+++ problem-api/src/main/java/com/distributed/problem/ProblemApplication.java
@@ -0,0 +1,14 @@
+package com.distributed.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemApplication.class, args);
+    }
+
+}
+
