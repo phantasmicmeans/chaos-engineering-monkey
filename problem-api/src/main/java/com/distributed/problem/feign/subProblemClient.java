027dda98da7d705782c6e18e2a83c5d2f038be77
phantasmicmeans <dlaak0630@naver.com>
2018-12-20 06:38:35 +0900

    fix api uri

--- problem-api/src/main/java/com/distributed/problem/feign/subProblemClient.java
+++ problem-api/src/main/java/com/distributed/problem/feign/subProblemClient.java
@@ -14,23 +14,23 @@ import java.util.List;
 @FeignClient(name = "SUB-PROBLEM", fallbackFactory = subProblemFallback.class)
 public interface subProblemClient {
 
-    @GetMapping(value = "api/v1/sub-problem/{id}", produces = {"application/json"})
+    @GetMapping(value = "v1/sub-problem/{id}", produces = {"application/json"})
     @ApiOperation(value = "id로 sub-problem조회", produces = "http")
     ResponseEntity getSubProblemById(@PathVariable("id") Long id);
 
-    @GetMapping(value = "api/v1/sub-problems/{code}", produces = {"application/json"})
+    @GetMapping(value = "v1/sub-problem/list/{code}", produces = {"application/json"})
     @ApiOperation(value = "code가 일치하는 sub-problem 전부 조회", protocols = "http")
     ResponseEntity <String> getSubProblemByCode(@PathVariable("code") String code);
 
-    @GetMapping(value = "api/v1/sub-problem/{code}/info/{id}", consumes = {"application/json"})
+    @GetMapping(value = "v1/sub-problem/{code}/info/{id}", consumes = {"application/json"})
     @ApiOperation(value = "code가 일치하는 sub-problem 중 index로 조회", protocols = "http")
     ResponseEntity <String> getSubProblemByCodeAndId(@PathVariable("code") String code, @PathVariable("id") Long id);
 
-    @PostMapping(value = "api/v1/sub-problem", produces = {"application/json"}, consumes = {"application/json"})
+    @PostMapping(value = "v1/sub-problem", produces = {"application/json"}, consumes = {"application/json"})
     @ApiOperation(value = "sub-problem 생성", protocols = "http")
     ResponseEntity <String> saveSubProblemByCode(@RequestBody subProblem subProblem);
 
-    @PostMapping(value = "api/v1/sub-problems", produces = {"application/json"}, consumes = {"application/json"})
+    @PostMapping(value = "v1/sub-problem/list", produces = {"application/json"}, consumes = {"application/json"})
     @ApiOperation(value = "sub-problem list 생성", produces = "http")
     @Headers("Content-Type: application/json")
     ResponseEntity <String> saveSubProblemsByCode(@RequestBody List<subProblem> subProblemList);
