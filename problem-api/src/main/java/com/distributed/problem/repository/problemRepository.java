edaed799776743d4810acb0c6124ffb9b4be64d4
phantasmicmeans <dlaak0630@naver.com>
2018-12-19 23:48:09 +0900

    fix return type collection to string, and fix jpa naming query findAllByCode -> findAllByCodeOrderByCode

--- problem-api/src/main/java/com/distributed/problem/repository/problemRepository.java
+++ problem-api/src/main/java/com/distributed/problem/repository/problemRepository.java
@@ -4,13 +4,12 @@ import com.distributed.problem.domain.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface problemRepository extends JpaRepository <Problem, Integer> {
 //
-    Collection<Problem> findAllByCode(String code); //code로  찾기
+    List<Problem> findAllByCodeOrderByCode(String code); //code로  찾기
     //Optional<Problem> findByIdAndCode(Integer id, String code); //code, id 로 찾기
 
 }
