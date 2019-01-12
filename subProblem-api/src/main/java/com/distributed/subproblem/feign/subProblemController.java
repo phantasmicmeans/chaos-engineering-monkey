1b3a6cdde322701b47d10cbea5bce52e313e0636
phantasmicmeans <dlaak0630@naver.com>
2019-01-12 23:48:23 +0900

    fix controller - use detached validator

--- subProblem-api/src/main/java/com/distributed/subproblem/feign/subProblemController.java
+++ subProblem-api/src/main/java/com/distributed/subproblem/feign/subProblemController.java
@@ -23,9 +23,10 @@ public class subProblemController implements subProblemClient{
 
     @Autowired
     private subProblemService subProblemService;
-
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private subProblemValidator subProblemValidator;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -33,7 +34,7 @@ public class subProblemController implements subProblemClient{
     public ResponseEntity getSubProblemById(Integer id) {
 
         logger.info("getSubProblemById(Long id)");
-        checkIsEmpty(id);
+        this.subProblemValidator.checkIsEmpty(id);
         subProblem subProblem = subProblemService.loadSubProblemById(id)
                             .orElseThrow(() -> new ResourceNotFoundException("cannot found sub-problems with that id"));
         logger.info("getSubProblemById return");
@@ -45,7 +46,7 @@ public class subProblemController implements subProblemClient{
     public ResponseEntity getSubProblemByCode(String code) {
 
         logger.info("getSubProblemByCode(String code)");
-        checkIsEmpty(code);
+        this.subProblemValidator.checkIsEmpty(code);
         List<subProblem> subProblems = subProblemService.loadSubProblemByCode(code);
         if(subProblems.isEmpty()) throw new ResourceNotFoundException("cannot found sub-problems with that code");
         logger.info("getSubProblemByCode return");
@@ -56,21 +57,22 @@ public class subProblemController implements subProblemClient{
     @Override
     public ResponseEntity getSubProblemByCodeAndId(String code, Long id) {
 
-        if(checkIsEmpty(code) && checkIsEmpty(id)){ }
+        if(this.subProblemValidator.checkIsEmpty(code) && this.subProblemValidator.checkIsEmpty(id)){ }
         return null;
     }
 
     @Override
     public ResponseEntity saveSubProblemByCode(subProblem subProblem) throws ClassCastException{
 
-        logger.info(subProblem.toString());
-        if(!checkIsEmpty(subProblem)) { throw new DataInvalidException("please check your data"); }
+        logger.info(subProblem.toString()); //to-do = null pointer check in validator with optional
+        if(!this.subProblemValidator.checkIsEmpty(subProblem)) { throw new DataInvalidException("please check your data"); }
 
         Optional<Object> object = Optional.ofNullable(subProblemService.saveSubProblem(subProblem));
         String error;
 
         Object retObject = object.get();
-        if(retObject.getClass().equals(subProblem.getClass())){ //save, redirect get uri 함께
+        if(object.get() instanceof subProblem) {
+        //if(retObject.getClass().equals(subProblem.getClass())){ //save, redirect get uri 함께
             URI location = ServletUriComponentsBuilder.fromCurrentServletMapping().path("api/v1/sub-problem/{id}")
                             .build().expand(subProblem.getId()).toUri();
 
@@ -91,7 +93,8 @@ public class subProblemController implements subProblemClient{
 
         Optional<Object> retObject = Optional.ofNullable(this.subProblemService.saveSubProblems(subProblemList));
 
-        if(retObject.get().getClass().equals(subProblemList.getClass())) { //List<subProblem이면
+        if(retObject.get() instanceof subProblem) {
+        //if(retObject.get().getClass().equals(subProblemList.getClass())) { //List<subProblem이면
             URI location = ServletUriComponentsBuilder.fromCurrentServletMapping().path("api/v1/sub-problems/{code}")
                     .build().expand(subProblemList.get(0).getCode()).toUri();
             return ResponseEntity.created(location).body(objectMapper.writeValueAsString(retObject.get()));
@@ -101,23 +104,4 @@ public class subProblemController implements subProblemClient{
         return ResponseEntity.status(500).body(objectMapper.writeValueAsString(retObject.get()));
     }
 
-    public boolean checkIsEmpty(Object object){
-
-        if(object.getClass().equals(String.class)) { //code - null, length check
-            String code = (String) Optional.ofNullable(object).orElseThrow(()
-                                -> new DataInvalidException("please check your {code} in uri or json data"));
-            return code.length() == 6;
-
-        }else if(object.getClass().equals(Integer.class)){ //id - null check
-            Integer Id = (Integer) Optional.ofNullable(object).orElseThrow(()
-                                -> new DataInvalidException("please check your {id} in uri or json data"));
-            return true;
-
-        }else if(object.getClass().equals(subProblem.class)){ //POST data
-            subProblem subProblem = (subProblem) Optional.ofNullable(object).orElseThrow(()
-                                -> new DataInvalidException("please check your json(post) data"));
-            return checkIsEmpty(subProblem.getCode()) && subProblem.getContent() != null; //null pointer
-        }
-        return false;
-    }
 }
